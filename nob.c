68bc124b05eb74281aa104c4940a6263afa7ff62
rexim <reximkut@gmail.com>
2025-11-03 10:21:04 +0700

    make cmd a global variable

--- nob.c
+++ nob.c
@@ -9,82 +9,84 @@
 #define THIRDPARTY_DIR "./thirdparty/"
 #define RAYLIB_DIR THIRDPARTY_DIR"raylib-5.0_linux_amd64/"
 
-void cflags(Cmd *cmd)
+Cmd cmd = {0};
+
+void cflags(void)
 {
-    cmd_append(cmd, "-Wall", "-Wextra", "-ggdb");
-    cmd_append(cmd, "-I"RAYLIB_DIR"include");
-    cmd_append(cmd, "-I"PANIM_DIR);
-    cmd_append(cmd, "-I.");
+    cmd_append(&cmd, "-Wall", "-Wextra", "-ggdb");
+    cmd_append(&cmd, "-I"RAYLIB_DIR"include");
+    cmd_append(&cmd, "-I"PANIM_DIR);
+    cmd_append(&cmd, "-I.");
 }
 
-void cc(Cmd *cmd)
+void cc(void)
 {
-    cmd_append(cmd, "cc");
-    cflags(cmd);
+    cmd_append(&cmd, "cc");
+    cflags();
 }
 
-void cxx(Cmd *cmd)
+void cxx(void)
 {
-    cmd_append(cmd, "g++");
-    cmd_append(cmd, "-Wno-missing-field-initializers"); // Very common warning when compiling raymath.h as C++
-    cflags(cmd);
+    cmd_append(&cmd, "g++");
+    cmd_append(&cmd, "-Wno-missing-field-initializers"); // Very common warning when compiling raymath.h as C++
+    cflags();
 }
 
-void libs(Cmd *cmd)
+void libs(void)
 {
-    cmd_append(cmd, "-Wl,-rpath="RAYLIB_DIR"lib/");
-    cmd_append(cmd, "-Wl,-rpath="PANIM_DIR);
-    cmd_append(cmd, "-L"RAYLIB_DIR"lib");
-    cmd_append(cmd, "-l:libraylib.so", "-lm", "-ldl", "-lpthread");
+    cmd_append(&cmd, "-Wl,-rpath="RAYLIB_DIR"lib/");
+    cmd_append(&cmd, "-Wl,-rpath="PANIM_DIR);
+    cmd_append(&cmd, "-L"RAYLIB_DIR"lib");
+    cmd_append(&cmd, "-l:libraylib.so", "-lm", "-ldl", "-lpthread");
 }
 
-bool build_plug_c(bool force, Cmd *cmd, const char *source_path, const char *output_path)
+bool build_plug_c(bool force, const char *source_path, const char *output_path)
 {
     int rebuild_is_needed = needs_rebuild1(output_path, source_path);
     if (rebuild_is_needed < 0) return false;
 
     if (force || rebuild_is_needed) {
-        cc(cmd);
-        cmd_append(cmd, "-fPIC", "-shared", "-Wl,--no-undefined");
-        cmd_append(cmd, "-o", output_path);
-        cmd_append(cmd, source_path);
-        libs(cmd);
-        return cmd_run(cmd);
+        cc();
+        cmd_append(&cmd, "-fPIC", "-shared", "-Wl,--no-undefined");
+        cmd_append(&cmd, "-o", output_path);
+        cmd_append(&cmd, source_path);
+        libs();
+        return cmd_run(&cmd);
     }
 
     nob_log(INFO, "%s is up-to-date", output_path);
     return true;
 }
 
-bool build_plug_cxx(bool force, Cmd *cmd, const char *source_path, const char *output_path)
+bool build_plug_cxx(bool force, const char *source_path, const char *output_path)
 {
     int rebuild_is_needed = needs_rebuild1(output_path, source_path);
     if (rebuild_is_needed < 0) return false;
 
     if (force || rebuild_is_needed) {
-        cxx(cmd);
-        cmd_append(cmd, "-fPIC", "-shared", "-Wl,--no-undefined");
-        cmd_append(cmd, "-o", output_path);
-        cmd_append(cmd, source_path);
-        libs(cmd);
-        return cmd_run(cmd);
+        cxx();
+        cmd_append(&cmd, "-fPIC", "-shared", "-Wl,--no-undefined");
+        cmd_append(&cmd, "-o", output_path);
+        cmd_append(&cmd, source_path);
+        libs();
+        return cmd_run(&cmd);
     }
 
     nob_log(INFO, "%s is up-to-date", output_path);
     return true;
 }
 
-bool build_exe(bool force, Cmd *cmd, const char **input_paths, size_t input_paths_len, const char *output_path)
+bool build_exe(bool force, const char **input_paths, size_t input_paths_len, const char *output_path)
 {
     int rebuild_is_needed = needs_rebuild(output_path, input_paths, input_paths_len);
     if (rebuild_is_needed < 0) return false;
 
     if (force || rebuild_is_needed) {
-        cc(cmd);
-        cmd_append(cmd, "-o", output_path);
-        da_append_many(cmd, input_paths, input_paths_len);
-        libs(cmd);
-        return cmd_run(cmd);
+        cc();
+        cmd_append(&cmd, "-o", output_path);
+        da_append_many(&cmd, input_paths, input_paths_len);
+        libs();
+        return cmd_run(&cmd);
     }
 
     nob_log(INFO, "%s is up-to-date", output_path);
@@ -111,12 +113,11 @@ int main(int argc, char **argv)
 
     if (!mkdir_if_not_exists(BUILD_DIR)) return 1;
 
-    Cmd cmd = {0};
-    if (!build_plug_c(force, &cmd, PLUGS_DIR"tm/plug.c", BUILD_DIR"libtm.so")) return 1;
-    if (!build_plug_c(force, &cmd, PLUGS_DIR"template/plug.c", BUILD_DIR"libtemplate.so")) return 1;
-    if (!build_plug_c(force, &cmd, PLUGS_DIR"squares/plug.c", BUILD_DIR"libsquare.so")) return 1;
-    if (!build_plug_c(force, &cmd, PLUGS_DIR"bezier/plug.c", BUILD_DIR"libbezier.so")) return 1;
-    if (!build_plug_cxx(force, &cmd, PLUGS_DIR"cpp/plug.cpp", BUILD_DIR"libcpp.so")) return 1;
+    if (!build_plug_c(force, PLUGS_DIR"tm/plug.c", BUILD_DIR"libtm.so")) return 1;
+    if (!build_plug_c(force, PLUGS_DIR"template/plug.c", BUILD_DIR"libtemplate.so")) return 1;
+    if (!build_plug_c(force, PLUGS_DIR"squares/plug.c", BUILD_DIR"libsquare.so")) return 1;
+    if (!build_plug_c(force, PLUGS_DIR"bezier/plug.c", BUILD_DIR"libbezier.so")) return 1;
+    if (!build_plug_cxx(force, PLUGS_DIR"cpp/plug.cpp", BUILD_DIR"libcpp.so")) return 1;
 
     {
         const char *output_path = BUILD_DIR"panim";
@@ -125,7 +126,7 @@ int main(int argc, char **argv)
             PANIM_DIR"ffmpeg_linux.c"
         };
         size_t input_paths_len = ARRAY_LEN(input_paths);
-        if (!build_exe(force, &cmd, input_paths, input_paths_len, output_path)) return 1;
+        if (!build_exe(force, input_paths, input_paths_len, output_path)) return 1;
     }
 
     return 0;
